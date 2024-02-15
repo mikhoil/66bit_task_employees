9adac16e029a48e8752e46ddc4abf8e5490ff7c7
lanskikh <lanskikh-mikhail@mail.ru>
2024-02-15 13:56:44 +0500

    changed vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => `src/app/assets/[name]-[hash].${assetInfo.name?.split('.')[1]}`,
+      }
+    }
+  }
 })
